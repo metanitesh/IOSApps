4ef2082a82050d4d51a1afc49232ca49140f3428
Nitesh Sharma <nitesh.grep@gmail.com>
2020-12-04 10:57:01 +0530

    adding main view

--- pitchPerfect/ViewController.swift
+++ pitchPerfect/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func recordButton(_ sender: Any) {
+        recordingLabel.text = "recording started"
+        print("hello")
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        recordingLabel.text = "recording stopped"
+    }
 }
 
